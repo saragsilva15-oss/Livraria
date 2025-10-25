0e0c59d11826144fbf092b98b34ace95ffe822f0
Gabriel F. <saragsilva15@aluno.senai.br>
2025-10-25 13:53:47 -0300

    exercicio finalizado

--- src/routes/autorRoute.js
+++ src/routes/autorRoute.js
@@ -1,11 +1,11 @@
 express = require("express");
 const router = express.Router();
 const { autorController } = require("../controllers/autorController");
-//GET /autor -> listar Autores
+//GET /autor -> listar Autor
 
 router.get('/autores',autorController.buscarAutor);
 
-//POST /autor -> cadastrar Autores
+//POST /autor -> cadastrar Autor
 
 router.post('/autores',autorController.cadastrarAutor);
 
